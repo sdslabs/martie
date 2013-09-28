3593f7e2bb1e8c3e862204cbc070642db05fe803
Abhay Rana <capt.n3m0@gmail.com>
2013-09-28 23:10:06 +0530

    Fixes indentation for YT module

--- youtube.js
+++ youtube.js
@@ -1,11 +1,11 @@
 var r=require("request");
 var apiRoot="https://www.googleapis.com/youtube/v3/";
 module.exports = {
-	search: function(str, cb){
-		var url = apiRoot+"search?q="+str+"&part=snippet&key=AIzaSyCzg8eQVsqLA5Tbwpk5ytGeZqzMgPlFBzg&maxResults=15";
-		r.get(url, function(err, res, body){
-			body = JSON.parse(body);
-			cb(body.items);
-		})
-	}
+  search: function(str, cb){
+    var url = apiRoot+"search?q="+str+"&part=snippet&key=AIzaSyCzg8eQVsqLA5Tbwpk5ytGeZqzMgPlFBzg&maxResults=15";
+    r.get(url, function(err, res, body){
+      body = JSON.parse(body);
+      cb(body.items);
+    })
+  }
 }
