7c3d2278af36288786b17c36c7c6deb321c53a4d
Abhay Rana <capt.n3m0@gmail.com>
2013-09-28 17:27:42 +0530

    Many changes. Added redis.

--- app.js
+++ app.js
@@ -4,9 +4,9 @@
 
 var express = require('express');
 var http = require('http');
-var path = require('path');
-
-var app = express();
+var app = express(),
+  redis = require("redis"),
+    r = redis.createClient();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -18,16 +18,45 @@ app.use(express.methodOverride());
 app.use(express.cookieParser('This is my secret'));
 app.use(express.session());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', function(req, res){
-	res.sendfile(__dirname + '/public/');
+app.post('/:party/add', function(){
+  if(req.session.admin){
+    
+  }
+  else{
+    res.json("You are not admin");
+  }
+})
+
+app.get('/:partyName', function(req, res){
+  //This is a party page
+  //send out different things to different people
+  if(req.session.admin){
+    res.sendfile("./public/admin.html");
+  }
+  else{
+    res.sendfile("./public/attendee.html");
+  }
 });
+
+app.get('/', function(req, res){
+  res.sendfile("./public/index.html");
+});
+
+app.post('/party/create', function(req,res){
+  var partyName = req.body.partyname;
+  var partyNameSanitized = partyName.replace(/\W/g,'-');
+  req.session.admin=true;
+  req.redirect('/'+partyNameSanitized);
+})
+
+app.use(express.static('./public'));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
