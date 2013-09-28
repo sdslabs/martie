47b0575aa98fe6fbacf50d51ba5885156fb0d12e
Abhay Rana <capt.n3m0@gmail.com>
2013-09-28 17:50:09 +0530

    Added some redis stuff. Party name still not resolving.

--- app.js
+++ app.js
@@ -26,14 +26,40 @@ if ('development' == app.get('env')) {
 }
 
 app.post('/:party/add', function(){
+  var trackId = req.body.trackId;
   if(req.session.admin){
-    
+
   }
   else{
     res.json("You are not admin");
   }
 })
 
+app.post('/:party/suggest', function(req, res){
+  var trackId = req.body.trackId;
+  var patyName = req.param.party
+  r.zadd("suggests:"+partyName, 1, trackId);
+});
+
+app.post('/:party/upvote', function(req, res){
+  var trackId = req.body.trackId;
+  var patyName = req.param.party
+  r.zincrby("suggests:"+partyName, 1, trackId);
+})
+
+/** This is the most important endpoint */
+app.get('/:partyName.json', function(req, res){
+  r.get("party:"+req.param.partyName, function(err, response){
+    console.log([err, response]);
+    if(err)
+      throw err;
+    console.log(response);
+    res.json({
+      name: response
+    })  
+  });
+  
+})
 app.get('/:partyName', function(req, res){
   //This is a party page
   //send out different things to different people
@@ -53,7 +79,8 @@ app.post('/party/create', function(req,res){
   var partyName = req.body.partyname;
   var partyNameSanitized = partyName.replace(/\W/g,'-');
   req.session.admin=true;
-  req.redirect('/'+partyNameSanitized);
+  r.set("party:"+partyNameSanitized, partyName);
+  res.redirect('/'+partyNameSanitized);
 })
 
 app.use(express.static('./public'));
