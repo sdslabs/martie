8b5130e5ee8a6fc3ec82dd23797f4bfc8a91b58a
Abhay Rana <capt.n3m0@gmail.com>
2013-09-29 13:51:12 +0530

    Soundmanager working for next track as well

--- public/javascripts/party.js
+++ public/javascripts/party.js
@@ -1,28 +1,28 @@
 window.currentTrackId =0 ;
 $(document).ready(function(){
   $.extend(soundManager.defaultOptions,{
-  /** What to do when the song ends */
-  onfinish:function(){
-    alert("song finished");
-    currentTrackId+=1;
-    window.play(currentTrackId);
-  }
-});
+    /** What to do when the song ends */
+    onfinish:function(){
+      currentTrackId=1+currentTrackId;
+      window.play(currentTrackId);
+    }
+  });
   soundManager.setup({ url: '/swf/', flashVersion: 9 ,
-  onready: function(){
-    window.play(0);
-  }});
+onready: function(){
+  window.play(0);
+}}
+  );
   window.play=function(i){
     var id = tracks[i].split("|")[0];
-    console.log("play called"+id);
     $.getJSON("http://localhost/muzi/ajax/track/?id="+id, function(data){
       window.currentTrack = soundManager.createSound({
        // optional id, for getSoundById() look-ups etc. If omitted, an id will be generated.
-       id: 'mySound',
+       id: 'mySound'+data.id,
        url: 'http://localhost/Music/'+data.file,
        // optional sound parameters here, see Sound Properties for full list
-       autoPlay: true
+       autoPlay: false,
       });
+      window.currentTrack.play();
     });
   }
 
